b618f1d0e67b5404790eebf2303a26ff8215306a
beauspot <beauspot@outlook.com>
2024-01-08 20:16:58 +0100

    took out the appConfig imports and imported the PrismaModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,17 +8,13 @@ import { UserModule } from './user/user.module';
 import { AuthController } from 'src/user/auth/auth.controller';
 import { AuthService } from './user/auth/auth.service';
 import { ConfigModule } from '@nestjs/config';
-import AppConfig from './config/app.config';
+import { PrismaModule } from './prisma/prisma.module';
+
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: ['.env'],
-      isGlobal: true,
-      ignoreEnvFile: false,
-      load: [AppConfig],
-    }),
     UserModule,
+    PrismaModule,
   ],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
