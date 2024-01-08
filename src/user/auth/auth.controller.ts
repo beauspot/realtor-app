74ad5ee32b4475f2b92b38cefe8da39a2188b6a6
beauspot <beauspot@outlook.com>
2024-01-08 20:21:32 +0100

    finished setting up the signup user services and controllers.

--- src/user/auth/auth.controller.ts
+++ src/user/auth/auth.controller.ts
@@ -1,15 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
 
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Put, Get, Delete, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignUpDTO } from 'src/dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
-    constructor (private readonly authService: AuthService){}
-    @Post('/signup')
-    signup() {
-        return this.authService.signup()
-     }
+  constructor(private readonly authService: AuthService) {}
+  @Post('/signup')
+  signup(@Body() body: SignUpDTO) {
+    return this.authService.signup(body);
+  }
 }
 //signup
--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,11 +1,51 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
 
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { SignUpParams } from 'src/interface/signupparams';
+import * as bcrypt from 'bcryptjs';
+import { UserType } from '@prisma/client';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class AuthService {
-    signup() {
-        return "User Signed Up";
+  constructor(private readonly prismaService: PrismaService) {}
+  async signup({ email, password, name, phone_no }: SignUpParams) {
+    // checking for the existence of the user via unique email
+    const userExists = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (userExists) {
+      throw new ConflictException();
     }
+
+    // Hashing the passwords
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // console.log({ hashedPassword });
+
+    const newUser = await this.prismaService.user.create({
+      data: {
+        email,
+        name,
+        phone_no,
+        password: hashedPassword,
+        user_type: UserType.BUYER,
+      },
+    });
+    const token = await jwt.sign(
+      {
+        id: newUser.id,
+        name,
+        email,
+      },
+      process.env.JSON_TOKEN_KEY,
+      {
+        expiresIn: 3600000,
+      },
+    );
+    return { token };
+  }
 }
