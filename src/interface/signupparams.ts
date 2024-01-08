cbaa2c478544c36db372fe9695089cf463d92348
beauspot <beauspot@outlook.com>
2024-01-08 20:18:25 +0100

    created the SignUpParams interface

---
+++ src/interface/signupparams.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+
+export interface SignUpParams {
+    email: string;
+    password: string;
+    name: string;
+    phone_no: string
+}
