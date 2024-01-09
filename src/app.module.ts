d5fabfb35cc3a8ff8e71a1a33f280b593555c301
beauspot <beauspot@outlook.com>
2024-01-09 11:29:27 +0100

    imported and registered the 'HomeModule' to the list of imports on the app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,14 @@ import { AuthController } from 'src/user/auth/auth.controller';
 import { AuthService } from './user/auth/auth.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { HomeModule } from './home/home.module';
 
 
 @Module({
   imports: [
     UserModule,
     PrismaModule,
+    HomeModule,
   ],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
