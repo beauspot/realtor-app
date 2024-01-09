d23b9a07bd59f9fe9aa37f1cb6afe6c417949aa4
beauspot <beauspot@outlook.com>
2024-01-09 03:49:04 +0100

    added a few lines of comments and finished setting up the sign in user feature and also implemented the generateJWT method inside the AuthService class.

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
 
-import { Injectable, ConflictException } from '@nestjs/common';
+import { Injectable, ConflictException, HttpException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { SignUpParams } from 'src/interface/signupparams';
+import { SignUpParams, SignInParams } from 'src/interface/signupparams';
 import * as bcrypt from 'bcryptjs';
 import { UserType } from '@prisma/client';
 import * as jwt from 'jsonwebtoken';
@@ -11,6 +11,7 @@ import * as jwt from 'jsonwebtoken';
 @Injectable()
 export class AuthService {
   constructor(private readonly prismaService: PrismaService) {}
+  // Signup Service for signing up user
   async signup({ email, password, name, phone_no }: SignUpParams) {
     // checking for the existence of the user via unique email
     const userExists = await this.prismaService.user.findUnique({
@@ -35,17 +36,47 @@ export class AuthService {
         user_type: UserType.BUYER,
       },
     });
-    const token = await jwt.sign(
+    const token = this.generateJWT(newUser.email, newUser.id);
+    return { user: { email }, tokenData: token };
+  }
+
+  // Signin the user
+  async signInUser({ email, password }: SignInParams) {
+    // checking for the existence of the user via the email
+    const user = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+    if (!user) throw new HttpException('Invalid credentials', 400);
+
+    const hashedPwd = user.password;
+
+    // comparing the passwords
+    const isValidPwd = await bcrypt.compare(password, hashedPwd);
+
+    if (!isValidPwd) throw new HttpException('Invalid credentials', 400);
+
+    const token = this.generateJWT(user.email, user.id);
+
+    return { user: { userEmail: email }, tokenData: token };
+  }
+
+  generate_product_key(email: string, userType: UserType){
+    const userStringToken = `${email}-${userType}-${process.env.PRODUCT_KEY_SECRET}`;
+
+    return bcrypt.hash(userStringToken, 10);
+  }
+
+  // Logic to generate JWT
+  private generateJWT(email: string, id: number) {
+    return jwt.sign(
       {
-        id: newUser.id,
-        name,
         email,
+        id,
       },
       process.env.JSON_TOKEN_KEY,
       {
         expiresIn: 3600000,
       },
     );
-    return { token };
   }
 }
