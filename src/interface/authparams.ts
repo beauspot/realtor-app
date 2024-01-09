f54e18379f440647d54158ae460286e512903548
beauspot <beauspot@outlook.com>
2024-01-09 03:50:59 +0100

    renamed this module to authparams.ts

---
+++ src/interface/authparams.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+
+export interface SignUpParams {
+  email: string;
+  password: string;
+  name: string;
+  phone_no: string;
+}
+
+export interface SignInParams {
+  email: string;
+  password: string;
+}
--- src/interface/signupparams.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable prettier/prettier */
-
-export interface SignUpParams {
-    email: string;
-    password: string;
-    name: string;
-    phone_no: string
-}
