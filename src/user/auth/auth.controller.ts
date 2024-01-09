891bb5f56c463dbd81f31ae41d71784d75d933fd
beauspot <beauspot@outlook.com>
2024-01-09 03:53:08 +0100

    Added additional imports statements on the signup and sign in route and also the key route and implemented validation procedures.

--- src/user/auth/auth.controller.ts
+++ src/user/auth/auth.controller.ts
@@ -1,16 +1,41 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
 
-import { Controller, Post, Put, Get, Delete, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Put,
+  Get,
+  Delete,
+  Body,
+  Param,
+  ParseEnumPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { SignUpDTO } from 'src/dto/auth.dto';
+import { SignUpDTO, SignInDTO, GenerateProductKeyDTO } from 'src/dto/auth.dto';
+import { UserType } from '@prisma/client';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-  @Post('/signup')
-  signup(@Body() body: SignUpDTO) {
+
+  @Post('/signup/:userType')
+  signup(
+    @Body() body: SignUpDTO,
+    @Param('userType', new ParseEnumPipe(UserType)) userType: UserType,
+  ) {
     return this.authService.signup(body);
   }
+
+  @Post('/signin')
+  signin(@Body() body: SignInDTO) {
+    return this.authService.signInUser(body);
+  }
+
+  @Post('/key')
+  // the body can be destructured
+  // {email, body}
+  generateProductKey(@Body() body: GenerateProductKeyDTO) {
+    return this.authService.generate_product_key(body.email, body.userType);
+  }
 }
-//signup
