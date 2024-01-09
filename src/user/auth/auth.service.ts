7b38e7f07dba32915fe508f8a67fcd4cd35855df
beauspot <beauspot@outlook.com>
2024-01-09 03:53:38 +0100

    still setting up the authService.

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -3,7 +3,7 @@
 
 import { Injectable, ConflictException, HttpException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { SignUpParams, SignInParams } from 'src/interface/signupparams';
+import { SignUpParams, SignInParams } from 'src/interface/authparams';
 import * as bcrypt from 'bcryptjs';
 import { UserType } from '@prisma/client';
 import * as jwt from 'jsonwebtoken';
