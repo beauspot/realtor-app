66542824e4cae5057073ef9634948a50bbf97142
beauspot <beauspot@outlook.com>
2024-01-17 16:38:21 +0100

    Simply implemented try catch block on most of the services.

--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
 
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { HomeResponseDTO } from 'src/dto/home.dto';
 import { PropertyType } from '@prisma/client';
@@ -25,57 +25,71 @@ export class HomeService {
 
   // Getting all homes
   async getHomes(filter: getHomesParams): Promise<HomeResponseDTO[]> {
-    const homes = await this.prismaService.home.findMany({
-      select: {
-        ...homeSelect,
-        images: {
-          select: {
-            url: true,
+    try {
+      const homes = await this.prismaService.home.findMany({
+        select: {
+          ...homeSelect,
+          images: {
+            select: {
+              url: true,
+            },
+            take: 1,
           },
-          take: 1,
         },
-      },
-      where: filter,
-    });
+        where: filter,
+      });
 
-    if (!homes.length) {
-      throw new NotFoundException();
+      if (!homes.length) {
+        throw new NotFoundException();
+      }
+
+      return homes.map((home) => {
+        const fetchHome = { ...home, image: home.images[0].url };
+        delete fetchHome.images;
+        return new HomeResponseDTO(fetchHome);
+      });
+    } catch (error) {
+      throw new HttpException(
+        { success: false, message: error.message },
+        error.status,
+      );
     }
-
-    return homes.map((home) => {
-      const fetchHome = { ...home, image: home.images[0].url };
-      delete fetchHome.images;
-      return new HomeResponseDTO(fetchHome);
-    });
   } // ?//city=Exampleville&minPrice=250000&maxPrice=300000&propertyType=RESIDENTIAL
 
   async getHomeById(id: number) {
-    const home = await this.prismaService.home.findUnique({
-      where: {
-        id,
-      },
-      select: {
-        ...homeSelect,
-        images: {
-          select: {
-            url: true,
+    try {
+      const home = await this.prismaService.home.findUnique({
+        where: {
+          id,
+        },
+        select: {
+          ...homeSelect,
+          images: {
+            select: {
+              url: true,
+            },
+          },
+          realtor: {
+            select: {
+              name: true,
+              email: true,
+              phone: true,
+            },
           },
         },
-        realtor: {
-          select: {
-            name: true,
-            email: true,
-            phone: true,
-          },
-        },
-      },
-    });
+      });
 
-    if (!home) {
-      throw new NotFoundException();
+      if (!home) {
+        throw new NotFoundException();
+      }
+
+      return new HomeResponseDTO(home);
+    } catch (error) {
+      throw new HttpException(
+        { success: false, message: error.message },
+        error.status,
+      );
     }
-
-    return new HomeResponseDTO(home);
   }
 
   async createHome(
@@ -91,55 +105,76 @@ export class HomeService {
     }: CreateHomeParams,
     userId: number,
   ) {
-    const home = await this.prismaService.home.create({
-      data: {
-        address,
-        number_of_bathrooms: numberOfBathrooms,
-        number_of_bedrooms: numberOfBedrooms,
-        city,
-        land_size: landSize,
-        propertyType,
-        price,
-        realtor_id: userId,
-      },
-    });
-    const homeImages = images.map((image) => {
-      return { ...image, home_id: home.id };
-    });
+    try {
+      const home = await this.prismaService.home.create({
+        data: {
+          address,
+          number_of_bathrooms: numberOfBathrooms,
+          number_of_bedrooms: numberOfBedrooms,
+          city,
+          land_size: landSize,
+          propertyType,
+          price,
+          realtor_id: userId,
+        },
+      });
+      const homeImages = images.map((image) => {
+        return { ...image, home_id: home.id };
+      });
 
-    await this.prismaService.image.createMany({ data: homeImages });
+      await this.prismaService.image.createMany({ data: homeImages });
 
-    return new HomeResponseDTO(home);
+      return new HomeResponseDTO(home);
+    } catch (error) {
+      throw new HttpException(
+        { success: false, message: error.message },
+        error.status,
+      );
+    }
   }
 
   async updateHomeById(id: number, data: updateHomeParams) {
-    const home = await this.prismaService.home.findUnique({
-      where: {
-        id,
-      },
-    });
-    if (!home) throw new NotFoundException();
+    try {
+      const home = await this.prismaService.home.findUnique({
+        where: {
+          id,
+        },
+      });
+      if (!home) throw new NotFoundException();
 
-    const updateHome = await this.prismaService.home.update({
-      where: {
-        id,
-      },
-      data,
-    });
-    return new HomeResponseDTO(updateHome);
+      const updateHome = await this.prismaService.home.update({
+        where: {
+          id,
+        },
+        data,
+      });
+      return new HomeResponseDTO(updateHome);
+    } catch (error) {
+      throw new HttpException(
+        { success: false, message: error.message },
+        error.status,
+      );
+    }
   }
 
   async deleteHomeById(id: number) {
-    await this.prismaService.image.deleteMany({
-      where: {
-        home_id: id,
-      },
-    });
-    await this.prismaService.home.delete({
-      where: {
-        id,
-      },
-    });
+    try {
+      await this.prismaService.image.deleteMany({
+        where: {
+          home_id: id,
+        },
+      });
+      await this.prismaService.home.delete({
+        where: {
+          id,
+        },
+      });
+    } catch (error) {
+      throw new HttpException(
+        { success: false, message: error.message },
+        error.status,
+      );
+    }
   }
 
   async getRealtorByHomeID(id: number) {
