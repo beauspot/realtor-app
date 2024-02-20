ec93e77cc507fef3fe44e265b60de1c4c16609ce
beauspot <beauspot@outlook.com>
2024-02-20 07:34:31 +0100

    implemented swagger for documentation on of the api on the backend of the application.

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from "@nestjs/common";
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 (async () => {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Realtor App')
+    .setDescription('Realtor Application service')
+    .setVersion('0.1')
+    .build();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
       transformOptions: {
-        enableImplicitConversion: true, 
-      }
-    })
+        enableImplicitConversion: true,
+      },
+    }),
   );
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 })();
